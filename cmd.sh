098ecc23323c87e5a64e9aa4cade516c96728812
naka-c1024 <ny42800824@gmail.com>
2021-12-12 12:31:06 +0900

    ADD: sampleを増やした,leak checkも行うスクリプトにした

--- cmd.sh
+++ cmd.sh
@@ -8,6 +8,8 @@ if [ ! $1 ]; then
 	echo "./cmd.sh b 42 text1.txt text2.txt"
 	echo "./cmd.sh d 42 text.txt"
 	echo "./cmd.sh 0 42"
+	echo "./cmd.sh lm 42 text.txt"
+	echo "./cmd.sh lb 42 text.txt"
 	exit 1
 elif [ $1 = "m" ]; then # mandatory
 	gcc -Wall -Wextra -Werror ../get_next_line.c \
@@ -37,4 +39,14 @@ elif [ $1 = "0" ]; then # stdin
 		../get_next_line_utils.c \
 		stdin_main.c -D BUFFER_SIZE="$2"
 	./a.out
+elif [ $1 = "lm" ]; then # leak check mandatory
+	gcc -g -Wall -Wextra -Werror ../get_next_line.c \
+		../get_next_line_utils.c \
+		mandatory_main.c -D BUFFER_SIZE="$2"
+	valgrind --leak-check=full ./a.out $3 && rm a.out
+elif [ $1 = "lb" ]; then # leak check bonus
+	gcc -g -Wall -Wextra -Werror ../get_next_line_bonus.c \
+	../get_next_line_utils_bonus.c \
+		bonus_main.c -D BUFFER_SIZE="$2"
+	valgrind --leak-check=full ./a.out $3 $4 && rm a.out
 fi
